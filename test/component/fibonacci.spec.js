cc3ef9a6d0a1695ca3c41ed100d0fd1ae20ebc1c
cellanda <git@cellanda.com>
2015-11-19 19:50:30 +0000

    initial

---
+++ test/component/fibonacci.spec.js
@@ -0,0 +1,22 @@
+var fibonacci = require('../../lib/fibonacci');
+
+describe('fibonacci', function () {
+
+
+	beforeEach(function () {
+	})
+
+
+	it('generates the first value', function () {
+		var value = fibonacci(1);
+		expect(value).to.equal(1);
+	});
+
+
+	it('generates the 6th value', function () {
+		var value = fibonacci(6);
+		expect(value).to.equal(8);
+	});
+
+
+});
