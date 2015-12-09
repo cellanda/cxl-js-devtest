50d80c4e131bfb833daaf7747024a864b828cff1
cellanda <git@cellanda.com>
2015-12-09 21:19:43 +0000

    give some isPrime unit tests

--- test/is-prime.spec.js
+++ test/is-prime.spec.js
@@ -7,4 +7,22 @@ describe('is prime', function () {
 	});
 
 
+	it('17 is prime', function () {
+		var value = isPrime(17);
+		expect(value).to.equal(true);
+	});
+
+
+	it('24 is not prime', function () {
+		var value = isPrime(24);
+		expect(value).to.equal(false);
+	});
+
+
+	it('15 is not prime', function () {
+		var value = isPrime(15);
+		expect(value).to.equal(false);
+	});
+
+
 });
